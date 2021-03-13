c55e0aeea7d0f158a41220eb2732ca84e04ce551
Michael Hutchings <67608821+MHutchbot@users.noreply.github.com>
2021-03-13 10:58:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose").set("debug", true);
+
+const path = require("path");
+require("dotenv").config();
+require("./services/passport.js");
+const compression = require("compression");
+const config = require("./config.js");
+
+if (process.env.NODE_ENV === 'production') {
+  
+}
+const env = process.env.NODE_ENV || "development";
+
+mongoose.connect(process.env.MONGODB_URI || 'mongod://localhost/my_database', {
+  useNewUrlParser: true,
+});
+
+const app = express();
+app.use(compression());
+
+env !== "development" &&
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+env === "development" && app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use("/api", require("./routes/router"));
+
+env !== "development" &&
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+
+app.listen(process.env.PORT || 5000);
